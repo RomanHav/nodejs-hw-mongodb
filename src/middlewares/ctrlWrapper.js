062c26bdded78a41b22c49de226c93ca4adae4f3
RomanHav <roman14082004@gmail.com>
2024-07-29 20:42:17 +0300

    fix wrapper

--- src/middlewares/ctrlWrapper.js
+++ src/middlewares/ctrlWrapper.js
@@ -1,7 +1,7 @@
 export const ctrlWrapper = (controller) => {
   return async (req, res, next) => {
     try {
-      await controller(req, res.next);
+      await controller(req, res, next);
     } catch (err) {
       next(err);
     }
